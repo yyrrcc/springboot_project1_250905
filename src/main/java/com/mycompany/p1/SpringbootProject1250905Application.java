95e41bb1c08b717e7abbe7e2c3a542f6d8128b4d
yyrrcc <music_v@naver.com>
2025-09-05 16:38:08 +0900

    h2 database setup

---
+++ src/main/java/com/mycompany/p1/SpringbootProject1250905Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject1250905Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject1250905Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/p1/SpringbootProject1250905ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject1250905ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
