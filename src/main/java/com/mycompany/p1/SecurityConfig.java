6af876a87c66c5c206f18644740e179fee258039
yyrrcc <music_v@naver.com>
2025-09-09 12:12:02 +0900

    Spring Security 설치

---
+++ src/main/java/com/mycompany/p1/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.mycompany.p1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration // 환경 설정 파일
+@EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션
+public class SecurityConfig {
+
+	@Bean // 스프링에 의해 생성 또는 관리되는 객체
+	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		// 인증되지 않은 모든 페이지의 요청을 허락한다는 의미 (로그인하지 않더라도 모든 페이지에 접근 가능)
+		httpSecurity.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+//		.csrf((csrf) -> csrf
+//                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))).headers((headers) -> headers
+//                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+//                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+		;
+		return httpSecurity.build();
+	}
+}
